6622d248686022380add40dec37edc30e014a618
Wellming Li <wellming.li@howzhi.com>
2016-07-24 15:03:21 +0800

    update queue

--- src/Queue/BeanstalkQueue.php
+++ src/Queue/BeanstalkQueue.php
@@ -6,7 +6,7 @@ use Codeages\Beanstalk\Client;
 
 class BeanstalkQueue implements Queue
 {
-    protected $client;
+    protected $client = null;
 
     const DEFAULT_PRI = 1000;
     const DEFAULT_DELAY = 0;
@@ -14,27 +14,57 @@ class BeanstalkQueue implements Queue
 
     public function __construct($options = array())
     {
-        $queue = new Client($options);
-        $queue->connect();
-        $this->queue = $queue;
+        $this->options = $options;
+    }
+
+    public function __destruct()
+    {
+        $this->disconnect();
+    }
+
+    public function connect($persistent = false)
+    {
+        $client = new Client(array_merge($this->options, ['persistent' => $persistent]));
+        $client->connect();
+        $this->client = $client;
+        return $this;
+    }
+
+    public function disconnect()
+    {
+        if ($this->client) {
+            $this->client->disconnect();
+            $this->client = null;
+        }
+        return $this;
     }
 
     public function push($queue, array $body, array $options = array())
     {
+        if (empty($this->client)) {
+            throw new \RuntimeException('Queue is not connected, please connect first.');
+        }
         $options = $this->fillOptions($options);
-        $this->queue->watch($queue);
-        return $this->queue->put($options['pri'], self::DEFAULT_DELAY, $options['ttr'], json_encode($body));
+        $this->client->useTube($queue);
+        return $this->client->put($options['pri'], self::DEFAULT_DELAY, $options['ttr'], json_encode($body));
     }
 
     public function pushDelay($queue, array $body, $delay, array $options = array())
     {
+        if (empty($this->client)) {
+            throw new \RuntimeException('Queue is not connected, please connect first.');
+        }
+
         $options = $this->fillOptions($options);
-        $this->queue->watch($queue);
-        $this->queue->put($options['pri'], $delay, $options['ttr'], json_encode($body));
+        $this->client->useTube($queue);
+        $this->client->put($options['pri'], $delay, $options['ttr'], json_encode($body));
     }
 
     public function pop($queue = null, $timeout = 0)
     {
+        if (empty($this->client)) {
+            throw new \RuntimeException('Queue is not connected, please connect first.');
+        }
 
     }
 
