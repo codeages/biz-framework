8576cd5bdf173b8620661db22bd7963eaf7ae5b1
Simon.liu <liuyangyang@howzhi.com>
2016-08-08 17:13:52 +0800

    add test for UnitTestsBootstrap

--- tests/Dao/MigrationBootstrapTest.php
+++ tests/Dao/MigrationBootstrapTest.php
@@ -3,14 +3,14 @@
 namespace Codeages\Biz\Framework\Tests\Dao;
 
 use Codeages\Biz\Framework\Tests\Example\ExampleKernel;
+use Codeages\Biz\Framework\UnitTests\UnitTestsBootstrap;
+
 use Codeages\Biz\Framework\Dao\MigrationBootstrap;
 
 class MigrationBootstrapTest extends \PHPUnit_Framework_TestCase
 {
-
     public function __construct()
     {
-
     }
 
     public function testRun()
@@ -22,6 +22,16 @@ class MigrationBootstrapTest extends \PHPUnit_Framework_TestCase
         $booted = $bootstrap->boot();
 
         $this->assertNotNull($booted);
+
+
     }
 
+    public function testUnitTestsBootstrap()
+    {
+        $kernel = new ExampleKernel();
+        $kernel->boot();
+
+        $unitTestsBootstrap = new UnitTestsBootstrap($kernel);
+        $unitTestsBootstrap->boot();
+    }
 }
