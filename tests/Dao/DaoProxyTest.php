69b82235b6b06a36153328a578daed141b6f8f53
Wellming Li <wellming.li@howzhi.com>
2017-04-01 22:26:55 +0800

    test

--- tests/Dao/DaoProxyTest.php
+++ tests/Dao/DaoProxyTest.php
@@ -12,84 +12,162 @@ class DaoProxyTest extends TestCase
 {
     public function testGetWithHitCache()
     {
-        $cache = array(
-            'id' => 1,
-            'name' => 'test',
-        );
-
-        $strategy = $this->prophesize('Codeages\Biz\Framework\Dao\CacheStrategy');
-        $strategy->beforeGet(
-            Argument::type('Codeages\Biz\Framework\Dao\GeneralDaoInterface'),
-            Argument::containingString('get'),
-            Argument::type('array')
-        )->willReturn($cache);
-
-        $dao = $this->prophesize('Codeages\Biz\Framework\Dao\GeneralDaoInterface');
-
-        $serializer = new FieldSerializer();
-
-        $biz = new Biz();
-        $biz['dao.cache.first.enabled'] = false;
-        $biz['dao.cache.second.enabled'] = true;
-        $biz['dao.cache.second.strategy.default'] = $strategy->reveal();
-
-        $proxy = new DaoProxy($biz, $dao->reveal(), $serializer);
-
-        $row1 = $proxy->get($cache['id']);
-        $row2 = $proxy->getByName($cache['name']);
-
-        $this->assertEquals($cache['id'], $row1['id']);
-        $this->assertEquals($cache['id'], $row2['id']);
-    }
-
-    public function testGetWithoutCache()
-    {
-        $expected = array(
-            'id' => 1,
-            'name' => 'test',
-        );
-
-        $strategy = $this->prophesize('Codeages\Biz\Framework\Dao\CacheStrategy');
-        $strategy->beforeGet(
-            Argument::type('Codeages\Biz\Framework\Dao\GeneralDaoInterface'),
-            Argument::containingString('get'),
-            Argument::type('array')
-        )->willReturn(false);
-        $strategy->afterGet(
-            Argument::type('Codeages\Biz\Framework\Dao\GeneralDaoInterface'),
-            Argument::containingString('get'),
-            Argument::type('array'),
-            Argument::type('array')
-        )->willReturn(null);
-
-        $dao = $this->prophesize('Codeages\Biz\Framework\Dao\GeneralDaoInterface');
-        $dao->declares()->willReturn(array());
-        $dao->get($expected['id'])->willReturn($expected);
-
-        $serializer = new FieldSerializer();
-
-        $biz = new Biz();
-        $biz['dao.cache.first.enabled'] = false;
-        $biz['dao.cache.second.enabled'] = true;
-        $biz['dao.cache.second.strategy.default'] = $strategy->reveal();
-
-        $proxy = new DaoProxy($biz, $dao->reveal(), $serializer);
-
+        $expected = array('id' => 1, 'name' => 'test',);
+        $proxy = $this->mockDaoProxyWithHitCache($expected, 'get');
         $row = $proxy->get($expected['id']);
 
         $this->assertEquals($expected['id'], $row['id']);
     }
 
-    public function testGetWithNoCache()
+    public function testFindWithHitCache()
     {
         $expected = array(
-            'id' => 1,
-            'name' => 'test',
+            array('id' => 1, 'name' => 'test 1',),
+            array('id' => 2, 'name' => 'test 2',),
         );
-        
+        $proxy = $this->mockDaoProxyWithHitCache($expected, 'find');
+        $rows = $proxy->find();
+
+        $this->assertEquals($expected, $rows);
+    }
+
+    public function testSearchWithHitCache()
+    {
+        $expected = array(
+            array('id' => 1, 'name' => 'test 1',),
+            array('id' => 2, 'name' => 'test 2',),
+        );
+        $proxy = $this->mockDaoProxyWithHitCache($expected, 'search');
+        $rows = $proxy->search();
+
+        $this->assertEquals($expected, $rows);
+    }
+
+    public function testCountWithHitCache()
+    {
+        $expected = 2;
+        $proxy = $this->mockDaoProxyWithHitCache($expected, 'count');
+        $count = $proxy->count();
+
+        $this->assertEquals($expected, $count);
+    }
+
+    public function testGetWithMissCache()
+    {
+        $expected = array('id' => 1, 'name' => 'test');
+        $proxy = $this->mockDaoProxyWithMissCache($expected, 'get');
+        $row = $proxy->get($expected['id']);
+
+        $this->assertEquals($expected, $row);
+    }
+
+    public function testSearchWithMissCache()
+    {
+        $expected = array(
+            array('id' => 1, 'name' => 'test 1',),
+            array('id' => 2, 'name' => 'test 2',),
+        );
+        $proxy = $this->mockDaoProxyWithMissCache($expected, 'search');
+        $row = $proxy->search(array(), array(), 0, 100);
+
+        $this->assertEquals($expected, $row);
+    }
+
+    public function testCountWithMissCache()
+    {
+        $expected = 2;
+        $proxy = $this->mockDaoProxyWithMissCache($expected, 'count');
+        $count = $proxy->count(array());
+
+        $this->assertEquals($expected, $count);
+    }
+
+    public function mockDaoProxyWithHitCache($expected, $proxyMethod)
+    {
+        $method = 'before'.ucfirst($proxyMethod);
+
+        $strategy = $this->prophesize('Codeages\Biz\Framework\Dao\CacheStrategy');
+        $strategy->$method(
+            Argument::type('Codeages\Biz\Framework\Dao\GeneralDaoInterface'),
+            Argument::any(),
+            Argument::type('array')
+        )->willReturn($expected);
+
+        $dao = $this->prophesize('Codeages\Biz\Framework\Dao\GeneralDaoInterface');
+
+        $serializer = new FieldSerializer();
+
+        $biz = new Biz();
+        $biz['dao.cache.first.enabled'] = false;
+        $biz['dao.cache.second.enabled'] = true;
+        $biz['dao.cache.second.strategy.default'] = $strategy->reveal();
+
+        return new DaoProxy($biz, $dao->reveal(), $serializer);
+    }
+
+    public function mockDaoProxyWithMissCache($expected, $proxyMethod)
+    {
+        $beforeMethod = 'before'.ucfirst($proxyMethod);
+        $afterMethod = 'after'.ucfirst($proxyMethod);
+
+        $strategy = $this->prophesize('Codeages\Biz\Framework\Dao\CacheStrategy');
+        $strategy->$beforeMethod(
+            Argument::type('Codeages\Biz\Framework\Dao\GeneralDaoInterface'),
+            Argument::type('string'),
+            Argument::type('array')
+        )->willReturn(false);
+        $strategy->$afterMethod(
+            Argument::type('Codeages\Biz\Framework\Dao\GeneralDaoInterface'),
+            Argument::type('string'),
+            Argument::type('array'),
+            Argument::any()
+        )->willReturn(null);
+
         $dao = $this->prophesize('Codeages\Biz\Framework\Dao\GeneralDaoInterface');
         $dao->declares()->willReturn(array());
-        $dao->get($expected['id'])->willReturn($expected);
+        $dao->$proxyMethod(Argument::cetera())->willReturn($expected);
+
+        $serializer = new FieldSerializer();
+
+        $biz = new Biz();
+        $biz['dao.cache.first.enabled'] = false;
+        $biz['dao.cache.second.enabled'] = true;
+        $biz['dao.cache.second.strategy.default'] = $strategy->reveal();
+
+        return new DaoProxy($biz, $dao->reveal(), $serializer);
+    }
+
+    public function testGetWithNoCache()
+    {
+        $expected = array('id' => 1, 'name' => 'test');
+        $proxy = $this->mockDaoProxyWithNoCache($expected, 'get');
+        $row = $proxy->get($expected['id']);
+        $this->assertEquals($expected, $row);
+    }
+
+    public function testSearchWithNoCache()
+    {
+        $expected = array('id' => 1, 'name' => 'test');
+        $proxy = $this->mockDaoProxyWithNoCache($expected, 'search');
+        $rows = $proxy->search(array(), array(), 0, 1);
+        $this->assertEquals($expected, $rows);
+    }
+
+    public function testCountWithNoCache()
+    {
+        $expected = 1;
+        $proxy = $this->mockDaoProxyWithNoCache($expected, 'count');
+        $count = $proxy->count(array());
+        
+        $this->assertEquals($expected, $count);
+    }
+
+    public function mockDaoProxyWithNoCache($expected, $proxyMethod)
+    {
+
+        $dao = $this->prophesize('Codeages\Biz\Framework\Dao\GeneralDaoInterface');
+        $dao->declares()->willReturn(array());
+        $dao->$proxyMethod(Argument::cetera())->willReturn($expected);
 
         $serializer = new FieldSerializer();
 
@@ -97,11 +175,7 @@ class DaoProxyTest extends TestCase
         $biz['dao.cache.first.enabled'] = false;
         $biz['dao.cache.second.enabled'] = false;
 
-        $proxy = new DaoProxy($biz, $dao->reveal(), $serializer);
-
-        $row = $proxy->get($expected['id']);
-
-        $this->assertEquals($expected['id'], $row['id']);
+        return new DaoProxy($biz, $dao->reveal(), $serializer);
     }
 
 }
