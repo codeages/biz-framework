1cab0cb54496e6c4f316bf9e06a21bb8a9350119
Wellming Li <wellming.li@howzhi.com>
2017-05-22 09:55:31 +0800

    unit tests

--- tests/Example/Tests/Dao/AnnotationExampleDaoTest.php
+++ tests/Example/Tests/Dao/AnnotationExampleDaoTest.php
@@ -27,6 +27,7 @@ class AnnotationExampleDaoTest extends IntegrationTestCase
 
     public function testGet_HitCache()
     {
+        // no data insert into database.
         $row = $this->seed('Tests\\Example\\Tests\\Seeder\\ExampleSeeder', false)->first();
         $this->redis->set($this->getPrimaryCacheKey($row['id']), $row);
 
@@ -53,6 +54,8 @@ class AnnotationExampleDaoTest extends IntegrationTestCase
 
     public function testGet_NotFound()
     {
+        $this->seed('Tests\\Example\\Tests\\Seeder\\ExampleSeeder');
+
         $cache = $this->redis->get($this->getPrimaryCacheKey(999));
         $this->assertFalse($cache);
 
@@ -61,6 +64,73 @@ class AnnotationExampleDaoTest extends IntegrationTestCase
 
         $cache = $this->redis->get($this->getPrimaryCacheKey(999));
         $this->assertNull($cache);
+    }
+
+    public function testGetByName_HitCache()
+    {
+        // no data insert into database.
+        $row = $this->seed('Tests\\Example\\Tests\\Seeder\\ExampleSeeder', false)->first();
+        $this->redis->set("dao:{$this->dao->table()}:getByName:{$row['name']}", $this->getPrimaryCacheKey($row['id']));
+        $this->redis->set($this->getPrimaryCacheKey($row['id']), $row);
+
+        $geted = $this->dao->getByName($row['name']);
+
+        $this->assertEquals($row['id'], $geted['id']);
+        $this->assertEquals($row['name'], $geted['name']);
+    }
+
+    public function testGetByName_MissCache()
+    {
+        $row = $this->seed('Tests\\Example\\Tests\\Seeder\\ExampleSeeder')->first();
+
+        $geted = $this->dao->getByName($row['name']);
+
+        $this->assertEquals($row['id'], $geted['id']);
+        $this->assertEquals($row['name'], $geted['name']);
+
+        $this->assertEquals($this->getPrimaryCacheKey($row['id']), $this->redis->get("dao:{$this->dao->table()}:getByName:{$row['name']}"));
+        $cache = $this->redis->get($this->getPrimaryCacheKey($row['id']));
+
+        $this->assertEquals($row['id'], $cache['id']);
+        $this->assertEquals($row['name'], $cache['name']);
+    }
+
+    public function testUpdate()
+    {
+        $row = $this->seed('Tests\\Example\\Tests\\Seeder\\ExampleSeeder')->first();
+        $this->redis->set($this->getPrimaryCacheKey($row['id']), $row);
+
+        $this->dao->update($row['id'], array('content' => 'updated_content'));
+
+        $this->assertFalse($this->redis->get($this->getPrimaryCacheKey($row['id'])));
+        $updated = $this->db->query("SELECT * FROM {$this->dao->table()} WHERE id = {$row['id']}")->fetch(\PDO::FETCH_ASSOC);
+        $this->assertEquals($row['id'], $updated['id']);
+        $this->assertEquals('updated_content',  $updated['content']);
+    }
+
+    public function testDelete()
+    {
+        $row = $this->seed('Tests\\Example\\Tests\\Seeder\\ExampleSeeder')->first();
+        $this->redis->set($this->getPrimaryCacheKey($row['id']), $row);
+
+        $this->dao->delete($row['id']);
+
+        $this->assertFalse($this->redis->get($this->getPrimaryCacheKey($row['id'])));
+
+        $deleted = $this->db->query("SELECT * FROM {$this->dao->table()} WHERE id = {$row['id']}")->fetch(\PDO::FETCH_ASSOC);
+        $this->assertFalse($deleted);
+    }
+
+    public function testCreate()
+    {
+        $row = $this->seed('Tests\\Example\\Tests\\Seeder\\ExampleSeeder', false)->first();
+
+        $this->dao->create($row);
+
+        $created = $this->db->query("SELECT * FROM {$this->dao->table()} WHERE id = {$row['id']}")->fetch(\PDO::FETCH_ASSOC);
+
+        $this->assertEquals($row['id'], $created['id']);
+        $this->assertEquals($row['name'], $created['name']);
     }
 
     protected function getPrimaryCacheKey($id)
