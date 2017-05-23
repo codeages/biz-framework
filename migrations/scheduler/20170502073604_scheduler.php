74e7baa4640cc80032ca975f32a105ce3198ca3d
nifeng <fengni@fengdeMacBook-Pro.local>
2017-05-23 22:32:19 +0800

    fix scheduler

--- migrations/scheduler/20170502073604_scheduler.php
+++ migrations/scheduler/20170502073604_scheduler.php
@@ -14,7 +14,7 @@ class Scheduler extends Migration
         $connection->exec("
             CREATE TABLE IF NOT EXISTS `job_pool` (
               `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
-              `name` varchar(1024) NOT NULL DEFAULT 'default' COMMENT '组名',
+              `name` varchar(128) NOT NULL DEFAULT 'default' COMMENT '组名',
               `max_num` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '最大数',
               `num` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '已使用的数量',
               `timeout` int(10) unsigned NOT NULL DEFAULT '0' COMMENT '执行超时时间',
@@ -27,11 +27,11 @@ class Scheduler extends Migration
         $connection->exec("
             CREATE TABLE IF NOT EXISTS `job` (
               `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '编号',
-              `name` varchar(1024) NOT NULL COMMENT '任务名称',
-              `pool` varchar(1024) NOT NULL DEFAULT 'default' COMMENT '所属组',
-              `source` varchar(1024) NOT NULL DEFAULT 'MAIN' COMMENT '来源',
-              `expression` varchar(1024) NOT NULL DEFAULT '' COMMENT '任务触发的表达式',
-              `class` varchar(1024) NOT NULL COMMENT '任务的Class名称',
+              `name` varchar(128) NOT NULL COMMENT '任务名称',
+              `pool` varchar(64) NOT NULL DEFAULT 'default' COMMENT '所属组',
+              `source` varchar(64) NOT NULL DEFAULT 'MAIN' COMMENT '来源',
+              `expression` varchar(128) NOT NULL DEFAULT '' COMMENT '任务触发的表达式',
+              `class` varchar(128) NOT NULL COMMENT '任务的Class名称',
               `args` text COMMENT '任务参数',
               `priority` int(10) unsigned NOT NULL DEFAULT 50 COMMENT '优先级',
               `pre_fire_time` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '任务下次执行的时间',
@@ -66,14 +66,14 @@ class Scheduler extends Migration
               `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '编号',
               `job_id` int(10) unsigned NOT NULL COMMENT '任务编号',
               `job_fired_id` int(10) unsigned NOT NULL DEFAULT 0 COMMENT '激活的任务编号',
-              `hostname` varchar(1024) NOT NULL DEFAULT '' COMMENT '执行的主机',
-              `name` varchar(1024) NOT NULL COMMENT '任务名称',
-              `pool` varchar(1024) NOT NULL DEFAULT 'default' COMMENT '所属组',
-              `source` varchar(1024) NOT NULL COMMENT '来源',
-              `class` varchar(1024) NOT NULL COMMENT '任务的Class名称',
+              `hostname` varchar(128) NOT NULL DEFAULT '' COMMENT '执行的主机',
+              `name` varchar(128) NOT NULL COMMENT '任务名称',
+              `pool` varchar(64) NOT NULL DEFAULT 'default' COMMENT '所属组',
+              `source` varchar(64) NOT NULL COMMENT '来源',
+              `class` varchar(128) NOT NULL COMMENT '任务的Class名称',
               `args` text COMMENT '任务参数',
               `priority` int(10) unsigned NOT NULL DEFAULT 50 COMMENT '优先级',
-              `status` varchar(1024) NOT NULL DEFAULT 'waiting' COMMENT '任务执行状态',
+              `status` varchar(32) NOT NULL DEFAULT 'waiting' COMMENT '任务执行状态',
               `created_time` int(10) unsigned NOT NULL COMMENT '任务创建时间',
               PRIMARY KEY (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
