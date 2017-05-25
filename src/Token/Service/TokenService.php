658e757902a63b10c96762e2d4da6fd2161d5101
Wellming Li <wellming.li@howzhi.com>
2017-05-25 22:46:24 +0800

    fix cs

--- src/Token/Service/TokenService.php
+++ src/Token/Service/TokenService.php
@@ -7,10 +7,10 @@ interface TokenService
     /**
      * 生成令牌
      *
-     * @param string $place 令牌使用场景
+     * @param string $place    令牌使用场景
      * @param array  $lifetime 令牌的有效时长（秒），`0`表示永久有效
-     * @param int $times 令牌可被校验的次数，超过该次数则校验失败
-     * @param mixed $data 令牌所属的业务数据
+     * @param int    $times    令牌可被校验的次数，超过该次数则校验失败
+     * @param mixed  $data     令牌所属的业务数据
      *
      * @throws GenerateException 当生成令牌的Key已存在时抛出异常
      *
@@ -22,7 +22,7 @@ interface TokenService
      * 校验令牌
      *
      * @param string $place 令牌使用场景
-     * @param string $key  令牌的Key
+     * @param string $key   令牌的Key
      *
      * @return bool 校验通过，返回`true`，否则返回`false`
      */
@@ -32,7 +32,7 @@ interface TokenService
      * 删除令牌
      *
      * @param string $place 令牌使用场景
-     * @param string $key  令牌的Key
+     * @param string $key   令牌的Key
      */
     public function destroy($place, $value);
 }
