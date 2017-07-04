315743051d858409f998ce20459160e3bf1cbfd3
ruanwenqin <ruanwenqin@howzhi.com>
2017-07-04 11:41:47 +0800

    移除注释

--- src/Dao/AdvancedDaoInterface.php
+++ src/Dao/AdvancedDaoInterface.php
@@ -6,11 +6,5 @@ interface AdvancedDaoInterface extends GeneralDaoInterface
 {
     public function batchCreate($rows);
 
-    /**
-     * $param $identifyColumn id
-     * @param $identifies array(1,2,3,4)
-     * @param $columnsList array(array('name' => '123', 'title' => '456'))
-     * @return mixed
-     */
     public function batchUpdate($identifies, $updateColumnsList, $identifyColumn = 'id');
 }
