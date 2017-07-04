154831aec519a52056e116014a152306787663b1
ruanwenqin <ruanwenqin@howzhi.com>
2017-07-04 16:47:20 +0800

    bathUpdateHelper增加一些方法

--- tests/Dao/BatchUpdateHelperTest.php
+++ tests/Dao/BatchUpdateHelperTest.php
@@ -32,6 +32,48 @@ class BatchUpdateHelperTest extends IntegrationTestCase
         $this->assertEquals('change_content_'.$examples[400]['id'], $examples[400]['content']);
     }
 
+    public function testGet()
+    {
+        $examples = array(
+            array('id' => 1, 'name' => 'name1'),
+            array('id' => 2, 'name' => 'name2'),
+            array('id' => 3, 'name' => 'name3'),
+            array('id' => 4, 'name' => 'name4'),
+            array('id' => 5, 'name' => 'name5'),
+            array('id' => 6, 'name' => 'name6'),
+        );
+
+        $helper = new BatchUpdateHelper($this->getAdvancedExampleDao());
+        foreach ($examples as $example) {
+            $update['name'] = $example['name'];
+            $helper->add('id', $example['id'], $update);
+        }
+
+        $updateFields = $helper->get('id', 5);
+        $this->assertEquals('name5', $updateFields['name']);
+    }
+
+    public function testFindIdentifyKeys()
+    {
+        $examples = array(
+            array('id' => 1, 'name' => 'name1'),
+            array('id' => 2, 'name' => 'name2'),
+            array('id' => 3, 'name' => 'name3'),
+            array('id' => 4, 'name' => 'name4'),
+            array('id' => 5, 'name' => 'name5'),
+            array('id' => 6, 'name' => 'name6'),
+        );
+
+        $helper = new BatchUpdateHelper($this->getAdvancedExampleDao());
+        foreach ($examples as $example) {
+            $update['name'] = $example['name'];
+            $helper->add('id', $example['id'], $update);
+        }
+
+        $identifyKeys = $helper->findIdentifyKeys('id');
+
+        $this->assertEquals(array(1,2,3,4,5,6), $identifyKeys);
+    }
 
     private function createBatchRecord($count)
     {
