d87e93f68b5a5b1307b988af534b85be4c3cce71
nifeng <nifeng@howzhi.com>
2017-09-01 10:08:11 +0800

    add cash flow when refunded

--- src/Pay/Service/Impl/PayServiceImpl.php
+++ src/Pay/Service/Impl/PayServiceImpl.php
@@ -214,6 +214,12 @@ class PayServiceImpl extends BaseService implements PayService
 
         $trade = $this->getPaymentTradeDao()->getByTradeSn($tradeSn);
 
+        $flow = $this->createSiteFlow($trade, array(), 'outflow');
+        if (!empty($trade['coin_amount'])) {
+            $flow = $this->createSiteFlow($trade, $flow, 'outflow', true);
+            $this->createUserFlow($trade, $flow, 'inflow', true);
+        }
+
         return $this->getTradeContext($trade['id'])->refunded();
     }
 
@@ -267,14 +273,6 @@ class PayServiceImpl extends BaseService implements PayService
 
     protected function createCashFlow($trade, $notifyData)
     {
-        if ('refund' == $trade['type']) {
-            $flow = $this->createSiteFlow($trade, array(), 'outflow');
-            if (!empty($trade['coin_amount'])) {
-                $flow = $this->createSiteFlow($trade, $flow, 'outflow', true);
-                $this->createUserFlow($trade, $flow, 'inflow', true);
-            }
-            return;
-        }
         $inflow = $this->createUserFlow($trade, array('amount' => $notifyData['pay_amount']), 'inflow');
         $outflow = $this->createUserFlow($trade, $inflow, 'outflow');
         $this->createSiteFlow($trade, $outflow, 'inflow');
