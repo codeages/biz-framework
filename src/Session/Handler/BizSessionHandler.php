e1e073d9e529cf50cd6a0bc4a10dcb1661c3028e
nifeng <nifeng@howzhi.com>
2017-09-26 14:11:56 +0800

    add session handler

---
+++ src/Session/Handler/BizSessionHandler.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Codeages\Biz\Framework\Session\Handler;
+
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
+
+class BizSessionHandler implements \SessionHandlerInterface
+{
+    protected $biz;
+    protected $lockers = array();
+    protected $storage;
+    protected $gcCalled = false;
+
+    protected $maxLifeTime = 86400;
+
+    public function __construct($container, TokenStorage $storage, $options = array())
+    {
+        $this->biz = $container->get('biz');
+        $this->storage = $storage;
+        if (!empty($options['max_life_time'])) {
+            $this->maxLifeTime = $options['max_life_time'];
+        }
+    }
+
+    public function close()
+    {
+        while ($locker = array_shift($this->lockers)) {
+            $this->releaseLock($locker);
+        }
+
+        if ($this->gcCalled) {
+            $this->getSessionService()->gc();
+        }
+
+        return true;
+    }
+
+    public function destroy($session_id)
+    {
+        $this->getSessionService()->deleteSessionBySessId($session_id);
+
+        return true;
+    }
+
+    public function gc($maxlifetime)
+    {
+        $this->gcCalled = true;
+    }
+
+    public function open($save_path, $name)
+    {
+        $this->gc($this->maxLifeTime);
+        return true;
+    }
+
+    public function read($session_id)
+    {
+        $this->lockers[] = $this->getLock($session_id);
+        if (!in_array($session_id, $this->lockers)) {
+            $this->lockers[] = $session_id;
+        }
+
+        $session = $this->getSessionService()->getSessionBySessId($session_id);
+
+        return $session['sess_data'];
+    }
+
+    public function write($session_id, $session_data)
+    {
+        $token = $this->storage->getToken();
+
+        if (empty($token) || ($token instanceof AnonymousToken) || !$token->getUser()) {
+            $userId = 0;
+        } else {
+            $userId = $token->getUser()->getId();
+        }
+
+        $session = $this->getSessionService()->getSessionBySessId($session_id);
+        $unsavedSession = array(
+            'sess_id' => $session_id,
+            'sess_data' => $session_data,
+            'sess_time' => time(),
+            'sess_lifetime' => $this->maxLifeTime,
+            'sess_user_id' => $userId,
+            'source' => 'web',
+        );
+        if (empty($session)) {
+            $this->getSessionService()->createSession($unsavedSession);
+        } else {
+            $this->getSessionService()->updateSessionBySessId($session_id, $unsavedSession);
+        }
+
+        return true;
+    }
+
+    public function getLock($lockName, $lockTime = 30)
+    {
+        $result = $this->biz['db']->fetchAssoc("SELECT GET_LOCK('sess_{$lockName}', {$lockTime}) AS getLock");
+
+        return $result['getLock'];
+    }
+
+    public function releaseLock($lockName)
+    {
+        $result = $this->biz['db']->fetchAssoc("SELECT RELEASE_LOCK('sess_{$lockName}') AS releaseLock");
+
+        return $result['releaseLock'];
+    }
+
+    private function getSessionService()
+    {
+        return $this->biz->service('Session:SessionService');
+    }
+}
