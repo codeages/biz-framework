4767a4d5f823c055e3e13f06e8a074885d1bfa5a
nifeng <nifeng@howzhi.com>
2017-09-27 14:13:48 +0800

    gc rename

--- src/Session/Job/OnlineGcJob.php
+++ src/Session/Job/OnlineGcJob.php
@@ -4,17 +4,11 @@ namespace Codeages\Biz\Framework\Session\Job;
 
 use Codeages\Biz\Framework\Scheduler\AbstractJob;
 
-class DeleteSessionJob extends AbstractJob
+class OnlineGcJob extends AbstractJob
 {
     public function execute()
     {
-        $this->getSessionService()->gc();
         $this->getOnlineService()->gc();
-    }
-
-    protected function getSessionService()
-    {
-        return $this->biz->service('Session:SessionService');
     }
 
     protected function getOnlineService()
