12fbba09f88bacecafdcc8612e2c14456920d94f
nifeng <nifeng@howzhi.com>
2017-09-27 14:22:46 +0800

    fix table create

--- tests/Fixtures/Migrations/20170924082411_biz_session_and_online.php
+++ tests/Fixtures/Migrations/20170924082411_biz_session_and_online.php
@@ -18,11 +18,11 @@ class BizSessionAndOnline extends Migration
               `sess_data` blob NOT NULL,
               `sess_time` int(10) unsigned NOT NULL,
               `sess_deadline` int(10) unsigned NOT NULL,
-              `created_time` int(10) NOT NULL,
+              `created_time` int(10) unsigned NOT NULL ,
               PRIMARY KEY (`id`),
               UNIQUE KEY `sess_id` (`sess_id`),
               INDEX sess_deadline (`sess_deadline`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
         ");
 
         $connection->exec("
@@ -36,12 +36,12 @@ class BizSessionAndOnline extends Migration
               `ip` varchar(32) NOT NULL DEFAULT '' COMMENT '客户端ip',
               `user_agent` varchar(1024) NOT NULL DEFAULT '',
               `source` VARCHAR(32) NOT NULL DEFAULT 'unknown' COMMENT '当前在线用户的来源，例如：app, pc, mobile',
-              `created_time` int(10) NOT NULL,
+              `created_time` int(10) unsigned NOT NULL,
               PRIMARY KEY (`id`),
               INDEX deadline (`deadline`),
               INDEX is_login (`is_login`),
               INDEX active_time (`active_time`)
-            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
         ");
     }
 
@@ -52,7 +52,7 @@ class BizSessionAndOnline extends Migration
     {
         $biz = $this->getContainer();
         $connection = $biz['db'];
-        $connection->exec('drop table `biz_session`');
-        $connection->exec('drop table `biz_online`');
+        $connection->exec("drop table `biz_session`");
+        $connection->exec("drop table `biz_online`");
     }
 }
