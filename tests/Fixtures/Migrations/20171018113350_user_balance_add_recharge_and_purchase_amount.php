2eb6dadefa989aa43be79e27d9a6d9d09a5ab830
bellorchid <zhongyunchang@yeah.net>
2017-10-18 12:01:13 +0800

    修改balance表的Class

--- tests/Fixtures/Migrations/20171018113350_user_balance_add_recharge_and_purchase_amount.php
+++ tests/Fixtures/Migrations/20171018113350_user_balance_add_recharge_and_purchase_amount.php
@@ -2,7 +2,7 @@
 
 use Phpmig\Migration\Migration;
 
-class UserBalanceAddRechargeAndPurchaseAmount extends Migration
+class BizUserBalanceAddRechargeAndPurchaseAmount extends Migration
 {
     /**
      * Do the migration
