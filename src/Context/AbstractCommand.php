e9fd749d0893c0a7602a7ae73f1740a745dab9a9
nifeng <nifeng@howzhi.com>
2017-10-23 13:35:29 +0800

    修改生成脚本是，匹配bug

--- src/Context/AbstractCommand.php
+++ src/Context/AbstractCommand.php
@@ -44,7 +44,7 @@ class AbstractCommand extends Command
 
         foreach ($finder as $file) {
             $path = $file->getRelativePathname();
-            if (substr($path, -strlen($name) - 4, -4) == $name) {
+            if (substr(substr($path, 15), 0, -4) == $name) {
                 return true;
             }
         }
