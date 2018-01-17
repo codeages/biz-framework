09da162a90d9c2a2a548eacd3a1ddfb670853652
chenwei <chenwei@howzhi.com>
2018-01-17 20:26:49 +0800

    修复，单元测试报错的问题: “

--- src/Utility/Str.php
+++ src/Utility/Str.php
@@ -102,7 +102,7 @@ class Str
         while (($len = strlen($string)) < $length) {
             $size = $length - $len;
             $bytes = random_bytes($size);
-            $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+            $string .= substr(str_replace(array('/', '+', '='), '', base64_encode($bytes)), 0, $size);
         }
         return $string;
     }
