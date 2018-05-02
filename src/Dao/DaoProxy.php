cbdb783639fb39be75c92591d05766046a8673c0
Wellming Li <wellming.li@howzhi.com>
2018-05-02 11:56:39 +0800

    UUID 模式下， Dao 输出 经过 decode 之后的 uid=501(Wellming) gid=20(staff) groups=20(staff),701(com.apple.sharepoint.group.1),12(everyone),61(localaccounts),79(_appserverusr),80(admin),81(_appserveradm),98(_lpadmin),501(access_bpf),33(_appstore),100(_lpoperator),204(_developer),250(_analyticsusers),395(com.apple.access_ftp),398(com.apple.access_screensharing),399(com.apple.access_ssh)。

--- src/Dao/DaoProxy.php
+++ src/Dao/DaoProxy.php
@@ -353,6 +353,11 @@ class DaoProxy
 
             $row[$key] = $this->serializer->unserialize($method, $row[$key]);
         }
+
+        $idGenerator = $this->getIdGenerator();
+        if (isset($row['id']) && $idGenerator) {
+            $row['id'] = $idGenerator->decode($row['id']);
+        }
     }
 
     protected function unserializes(array &$rows)
